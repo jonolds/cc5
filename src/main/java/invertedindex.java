a092fa13f9ff5e9b8eea76387c20a0c259245498
jonolds <jonolds@gmail.com>
2019-03-15 09:11:59 -0500

    win

--- src/main/java/invertedindex.java
+++ src/main/java/invertedindex.java
@@ -4,7 +4,7 @@ import java.util.Map.Entry;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.IntWritable; 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
